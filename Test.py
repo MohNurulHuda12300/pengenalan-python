79e7ebc7dd1866043b498992cd2a0f7af62bf694
MohNurulHuda12300 <80238549+MohNurulHuda12300@users.noreply.github.com>
2022-05-09 23:07:42 +0700

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,11 @@
+"""EKSEKUSI MULAI DARI ATAS KE BAWAH"""
+
+print("apa kabar beb? ")
+# ini namanya Comment
+a = 10 #comment juga bisa ditaruh disini 
+b = 30 #comment biasanya dipakai untuk menandai programnya
+print(a) 
+print(b)
+""" tanda titik tiga ini juga disebut comment"""
+
+#UNTUK MEMINDAHKAN BARIS TEKAN ALT  di awal baris lalu bersamaan tekan arah atas atau bawah 
