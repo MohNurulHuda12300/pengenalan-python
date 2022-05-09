e40b4d1ac749786c71ae05ad7428b067db2b2e03
MohNurulHuda12300 <80238549+MohNurulHuda12300@users.noreply.github.com>
2022-05-09 22:34:59 +0700

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,27 @@
+""" Variable """
+#variable adalah tempat untuk menyimpan data
+
+#Langkah 1 kegiatan untuk menaruh nilai.
+a = 10
+# a disebut sebagai variable
+# 10 disebut dengan value/nilai
+print(a)
+#lalu kita bisa panggil seperti di atas.
+print("Jadi nilai a adalah", a)
+#kita juga bisa menambahkan text saat memanggil variable
+
+#Langkah 2 Penamaan
+nilai_y = 10 #ketika membuat variable tidak boleh menggunakan spasi, jadi gunakan underscore
+nilaiy = 20 #ini juga boleh
+juta10 = 100000 #tidak boleh menaruh angka di depan variable, tetapi jika dibelakang variable boleh
+
+""" Pemanggilan """
+#pemanggilan pertama
+print(a)
+
+a = 7 # a boleh dirubah nilainya
+#pemanggilan kedua
+print("nilai a yang tlah berubah ", a) #akan menghasilkan 7 karna sudah dirubah nlainya
+
+b = a
+print("jadi b = ", b)
